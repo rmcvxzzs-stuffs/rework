2c6927e6b7d5ed9a35483e9404123b58008e90da
rmchekzz <remcheckzz@gmail.com>
2025-08-17 19:06:16 +0700

    Update announce.sh

--- scripts/announce.sh
+++ scripts/announce.sh
@@ -1,5 +1,16 @@
-# dont make an announcement yet
-curl -X POST http://localhost:8080/announcements \
-     -H "Content-Type: application/json" \
-     -H "X-Admin-Token: [replace]" \
-     -d '{"platform":"web","language_code":"en","subject":"[replace]","text":"[replace]"}'
+#!/bin/sh
+
+# Prompt user for input
+read -sp "Enter ADMIN_SECRET_TOKEN: " ADMIN_SECRET_TOKEN
+echo
+read -p "Enter announcement SUBJECT: " ANNOUNCEMENT_SUBJECT
+read -p "Enter announcement TEXT: " ANNOUNCEMENT_TEXT
+
+# CHANGE SERVER ENDPOINT TO YOUR LIKING
+SERVER_URL="http://localhost:8080/announcements"
+
+# Send POST request with curl
+curl -X POST "$SERVER_URL" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $ADMIN_SECRET_TOKEN" \
+  -d "{\"subject\": \"$ANNOUNCEMENT_SUBJECT\", \"text\": \"$ANNOUNCEMENT_TEXT\"}"
