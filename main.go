d5032cbab2c52edf88b0a4fe2a563e673328a0c7
rmchekzz <remcheckzz@gmail.com>
2025-08-18 12:53:24 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
+package main
+
 import (
-    "github.com/gofiber/fiber/v2"
+	"database/sql"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-    // Fiber instance
-    app := fiber.New(fiber.Config{
-	CaseSensitive:  true
-	ServerHeader:   "Fiber",	
-	AppName:   "Horizon" // app name btw
-})
-    
+	cfg, err := LoadConfig("config.json")
+	if err != nil {
+		log.Fatal("Failed to load config:", err)
+	}
+	db, err := sql.Open("mysql", cfg.MySQL.DSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	app := fiber.New()
+	RegisterAnnouncementRoutes(app, db)
+    AppName: "Rework",
+
+	// Add other route registrations here (i.e., matchmaking, lobby, etc.)
+
+	log.Fatal(app.Listen(":8080"))
+}
